816ec86b28c206abe8d472c7fdfeade58d549f5d
milliax <wei@sivir.pw>
2021-07-13 11:05:49 +0800

    add template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/main.css'
+import '../styles/fontawesome-all.min.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <Component {...pageProps} />
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/jquery.min.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/jquery.scrolly.min.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/jquery.scrollex.min.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/browser.min.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/breakpoints.min.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/util.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/main.js`}/>
+        </>
+    )
 }
 
 export default MyApp
