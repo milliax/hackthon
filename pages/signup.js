edeb91aa17cc6289da314466565a43234c1e6a00
alias722 <wei@sivir.pw>
2021-07-13 23:16:22 +0800

    dont remove my commit plz

--- pages/signup.js
+++ pages/signup.js
@@ -38,7 +38,7 @@ export default function Signup(){
             return
         }
         try{
-            const res = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/api/posts/register`,{
+            const res = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/register`,{
                 method: 'POST',
                 cors: 'no-cors',
                 headers: {
