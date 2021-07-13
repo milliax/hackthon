0fb5196fd3f27fbd0f3420d4a085060ea045384d
milliax <wei@sivir.pw>
2021-07-13 11:20:12 +0800

    another page

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,19 @@
+import React from 'react'
 import '../styles/main.css'
 import '../styles/fontawesome-all.min.css'
 
 function MyApp({Component, pageProps}) {
     return (
-        <>
+        <React.Fragment>
             <Component {...pageProps} />
-            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/jquery.min.js`}/>
-            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/jquery.scrolly.min.js`}/>
-            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/jquery.scrollex.min.js`}/>
-            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/browser.min.js`}/>
-            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/breakpoints.min.js`}/>
-            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/util.js`}/>
-            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/assets/js/main.js`}/>
-        </>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/hackthon/assets/js/jquery.min.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/hackthon/assets/js/jquery.scrolly.min.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/hackthon/assets/js/jquery.scrollex.min.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/hackthon/assets/js/browser.min.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/hackthon/assets/js/breakpoints.min.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/hackthon/assets/js/util.js`}/>
+            <script src={`${process.env.NEXT_PUBLIC_DATABASE}/hackthon/assets/js/main.js`}/>
+        </React.Fragment>
     )
 }
 
---
+++ pages/another.js
@@ -0,0 +1,12 @@
+export default function Anothor(){
+    return(
+        <div id="wrapper">
+            <header id="header" className="alt">
+                <a href="/" className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></a>
+                <nav>
+                    <a href="#menu">Menu</a>
+                </nav>
+            </header>
+        </div>
+    )
+}
