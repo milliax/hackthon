a9ce2896d9a49e5bb5a725bbe781fb2af7517ad9
CSY54 <alex35748954@gmail.com>
2021-07-14 20:51:02 +0800

    add UI Improvement

--- component/Navigation.js
+++ component/Navigation.js
@@ -1,6 +1,34 @@
 import Link from 'next/link'
+import Cookies from "universal-cookie";
+import {useEffect, useRef, useState} from "react";
+import Swal from "sweetalert2";
 
 export default function Navigation() {
+    const cookies = new Cookies()
+    const [loggedIn, setLoggedIn] = useState(true)
+    const isInitState = useRef(true)
+
+    useEffect(()=>{
+        if(isInitState.current){
+            isInitState.current = false
+            let ck = cookies.getAll()
+            if(ck['access_token']){
+                setLoggedIn(true)
+            } else {
+                setLoggedIn(false)
+            }
+        }
+    })
+
+    function logout(){
+        cookies.set('access_token',null)
+        Swal.fire({
+            icon: "success",
+            title: "成功",
+            text: "登出成功"
+        })
+    }
+
     return (
         <nav id="menu">
             <ul className="links">
@@ -9,8 +37,18 @@ export default function Navigation() {
                 <li><Link href="/report">回報問題</Link></li>
             </ul>
             <ul className="actions stacked">
-                <li><a href="/login" className="button primary fit">登入</a></li>
-                <li><a href="/signup" className="button fit">註冊</a></li>
+                {
+                    (!loggedIn)? (
+                        <>
+                            <li><a href="/login" className="button primary fit">登入</a></li>
+                            <li><a href="/signup" className="button fit">註冊</a></li>
+                        </>
+                    ) : (
+                        <>
+                            <li><button className="button primary fit" onClick={logout}>登出</button></li>
+                        </>
+                    )
+                }
             </ul>
         </nav>
     )
